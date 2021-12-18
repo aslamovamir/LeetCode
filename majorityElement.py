009e95bc9fbd36f13698c975518ed665d3dfc323
Amir Aslamov <90361791+aslamovamir@users.noreply.github.com>
2021-12-17 21:10:55 -0500

    Create majorityElement.py

---
+++ majorityElement.py
@@ -0,0 +1,25 @@
+# Given an array nums of size n, return the majority element.
+
+# The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+# Example 1:
+
+# Input: nums = [3,2,3]
+# Output: 3
+# Example 2:
+
+# Input: nums = [2,2,1,1,1,2,2]
+# Output: 2
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        FreqDict = {}
+        majority = len(nums)/2
+        for num in nums:
+            if num in FreqDict:
+                FreqDict[num] += 1
+            else:
+                FreqDict[num] = 1
+            
+        for key, value in FreqDict.items():
+            if value > majority:
+                return key
