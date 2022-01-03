b7b88b02fd1cade9f645ce319f18164fe6a298ed
Amir Aslamov <90361791+aslamovamir@users.noreply.github.com>
2022-01-02 23:55:20 -0500

    Create Monotonic_array.py

---
+++ Monotonic_array.py
@@ -0,0 +1,28 @@
+# An array is monotonic if it is either monotone increasing or monotone decreasing.
+
+# An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j]. An array nums is monotone decreasing if for all i <= j, nums[i] >= nums[j].
+
+# Given an integer array nums, return true if the given array is monotonic, or false otherwise.
+
+# Example 1:
+
+# Input: nums = [1,2,2,3]
+# Output: true
+# Example 2:
+
+# Input: nums = [6,5,4,4]
+# Output: true
+# Example 3:
+
+# Input: nums = [1,3,2]
+# Output: false
+
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        increasing = sorted(nums)
+        decreasing = sorted(nums, reverse=True)
+        
+        if nums == sorted(nums) or nums == sorted(nums, reverse=True):
+            return True
+        else:
+            return False
