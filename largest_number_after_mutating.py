ecf4503234713a5149f1a1b3d6968952c4c213a8
Amir Aslamov <90361791+aslamovamir@users.noreply.github.com>
2022-02-01 10:30:22 -0500

    Update largest_number_after_mutating.py

--- largest_number_after_mutating.py
+++ largest_number_after_mutating.py
@@ -34,35 +34,44 @@
 # Output: "5"
 # Explanation: "5" is already the largest number that can be created, so return it.
 
-
-class Solution:
-    def maximumNumber(self, num: str, change: List[int]) -> str:
-        answer = ""
-        progress = True
-        swapped = False
-        for i in range(len(num)):
-            if int(num[i]) < change[int(num[i])]:
-                if progress:
-                    swapped = True
-                    answer += str(change[int(num[i])])
-                else:
-                    answer += num[i]
-            elif int(num[i]) == change[int(num[i])]:
-                answer += str(change[int(num[i])])  
-            else:
-                if progress and swapped:
-                    progress = False
-                answer += num[i]
-        
-        return answer
-    
-        
-      
- 
-
 # Constraints:
 
 # 1 <= num.length <= 105
 # num consists of only digits 0-9.
 # change.length == 10
 # 0 <= change[d] <= 9
+
+class Solution:
+    def maximumNumber(self, num: str, change: List[int]) -> str:
+        
+        answer = ""
+        # progress will help to dtermine if we are swapping in substring
+        progress = True
+        # swapped will help in determining if we ever made a swap, and therefore if 
+        # we initiated a substring
+        swapped = False
+        
+        for i in range(len(num)):
+            # if the digit from left is smalled than its hash value, we see if we can make a change
+            if int(num[i]) < change[int(num[i])]:
+                # we can swap if we are in a substring, and have been swapping consistently
+                if progress:
+                    # we initiated the substring
+                    swapped = True
+                    answer += str(change[int(num[i])])
+                else:
+                    # but if we are no longer in a substring, we do not make a swap
+                    answer += num[i]
+            # if the digit and the hash value are the same, we just get the digit itslef
+            elif int(num[i]) == change[int(num[i])]:
+                answer += str(change[int(num[i])])  
+            # if the digit is greater than the hash value, we just take the digit
+            else:
+                # if we have been swapping digits in a substring, we indicate that we are no longer in
+                # a substring
+                if progress and swapped:
+                    progress = False
+                answer += num[i]
+        
+        return answer
+       
